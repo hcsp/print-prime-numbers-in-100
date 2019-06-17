d7eb6d6f55adc6066a9ad1f6c261fe3f273cac5b
BeckPond <49722748+BeckPond@users.noreply.github.com>
2019-06-17 13:42:41 +0800

    Update Main.java

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -23,7 +23,7 @@ public class Main {
         if(number==2) {
             return true;
         }else{
-            for (int i = 1; i < Math.sqrt(number) + 1; i++) {
+            for (int i = 2; i < Math.sqrt(number) + 1; i++) {
                 if (number % i == 0) {
                     return false;
                 }
