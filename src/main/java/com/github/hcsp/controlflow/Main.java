7203982d9deb4059c5e0adfd347f7185df86b28e
HBhakunamatata <59411479+HBhakunamatata@users.noreply.github.com>
2020-12-19 20:45:32 +0800

    print prime number (#25)
    print prime number (#25)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -10,5 +10,20 @@ public class Main {
      *
      * <p>提示：对于正整数n，如果用2到Math.sqrt(n)+1之间的所有整数去除，均无法整除，则n为质数。
      */
-    public static void printPrimeNumbers() {}
+    public static void printPrimeNumbers() {
+        for (int i = 1; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + "");
+            }
+        }
+    }
+
+    public static boolean isPrime (int num) {
+        for (int i = 2; i < Math.sqrt(num) + 1; i++) {
+            if (num % i == 0 && num != i) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
